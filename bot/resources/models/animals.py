fde5eaaf8c51269ba162bfda8a2ea5fc32696dfd
Axel Sparkster <axelsparkster@gmail.com>
2024-08-22 21:58:58 -0500

    Update animals.py

--- bot/resources/models/animals.py
+++ bot/resources/models/animals.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
 
-ANIMAL_LITERAL = Literal["fox", "yeen", "dog", "snek", "poss", "leo", "serval", "bleat",
+ANIMAL_LITERAL = Literal["fox", "yeen", "dog", "manul", "snek", "poss", "serval", "bleat",
                          "shiba", "racc", "dook", "ott", "snep", "woof", "capy", "bear", "bun",
-                         "caracal", "puma", "mane", "marten", "tig", "skunk", "jaguar", "yote"]
+                         "caracal", "puma", "mane", "marten", "wah", "skunk", "jaguar", "yote"]
